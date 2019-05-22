7125cb42fa90f961d016e83b8c41cdc66c858d18
Alisson Vargas <alisson.vargass@hotmail.com>
2019-05-22 00:06:19 -0300

    sum_cards done

--- cards.py
+++ cards.py
@@ -1,11 +1,11 @@
 class Card():
     def set_value(self):
         if self.rank in ['JACK', 'QUEEN', 'KING']:
-            self.value = [10]
+            self.value = 10
         elif self.rank == 'Ace':
-            self.value = [1, 11]
+            self.value = 11
         else:
-            self.value = [int(self.rank)]
+            self.value = int(self.rank)
 
     def __init__(self, suit, rank):
         self.suit = suit
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from random import shuffle
 from cards import Card
 
 def bet(balance):
+    '''
+    Initial bet for the game
+    '''
     while True:
         try:
             bet_amount = int(input('How much do you want to bet? '))
@@ -45,7 +48,7 @@ def show_hands(machine_hand, player_hand, player_turn=True):
     '''
     Show all the cards in the player and machines' hand
     '''
-    print('==== Machine Hand ====')
+    print('\n==== Machine Hand ====')
     if player_turn:
         print('[Face Down]')
         print(machine_hand[1])
@@ -57,6 +60,22 @@ def show_hands(machine_hand, player_hand, player_turn=True):
     for card in player_hand:
         print(card)
 
+def sum_cards(hand):
+    '''
+    Sum the cards values in the hand
+    '''
+    total = 0
+    for card in hand:
+        total += card.value
+
+    #changes the value of the first Ace to '1' if the player is busted
+    #and if he's still busted rerun sum_cards
+    aces = [card for card in hand if card.value == 11]
+    if total > 21 and aces != []:
+        aces[0].value = 1
+        total = sum_cards(hand)
+    
+    return total
 
 if __name__ == '__main__':
     balance = 100
@@ -70,6 +89,6 @@ if __name__ == '__main__':
     for x in range(2):
         machine_hand = hit(machine_hand, deck)
         player_hand = hit(player_hand, deck)
-
+    
     show_hands(machine_hand, player_hand)
-        
+    print('TOTAL: ' + str(sum_cards(player_hand)))
